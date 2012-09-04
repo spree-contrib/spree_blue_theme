22aaf323842744954903d54026bcdbfb97f2ed2b
Konstantinos Servis <knservis@gmail.com>
2012-09-04 13:49:59 +1000

    store_menu seems to be gone

--- app/overrides/add_class_to_link_to_cart.rb
+++ app/overrides/add_class_to_link_to_cart.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_store_menu',
-                     :name => 'add_class_to_link_to_cart',
-                     :set_attributes => '#link-to-cart[data-hook]',
-                     :attributes => { :class => 'cart-indicator' })
+#Deface::Override.new(:virtual_path => 'spree/shared/_store_menu',
+#                     :name => 'add_class_to_link_to_cart',
+#                     :set_attributes => '#link-to-cart[data-hook]',
+#                     :attributes => { :class => 'cart-indicator' })
 
